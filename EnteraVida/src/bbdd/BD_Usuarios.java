3df3b63b9e0bdae7198a10dab9fcf6219a8936fc
admin <admin@Win7_1TB>
2019-04-25 13:41:32 +0300

    Inicial

---
+++ EnteraVida/src/bbdd/BD_Usuarios.java
@@ -0,0 +1,14 @@
+/*
+ * BD_Usuarios: Operaciones de añadir,buscar y eliminar usuarios de la base de datos.
+ */
+
+package bbdd;
+
+public class BD_Usuarios extends BD_Conector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
