9e5472043239db38977aaac2504c3c0925614b76
daw1 <daw1@ubuntu>
2019-05-07 11:04:01 +0200

    Creacion de tablas Noticias y Temas con sus respectivos métodos.

--- EnteraVida/src/bbdd/BD_Noticias.java
+++ EnteraVida/src/bbdd/BD_Noticias.java
@@ -1,5 +1,101 @@
 package bbdd;
 
-public class BD_Noticias {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import modelos.Noticias;
+import modelos.Usuarios;
+
+
+public class BD_Noticias extends BD_Conector{
+	private static Statement s;		
+	private static ResultSet reg;
+
+	public BD_Noticias(){
+		super();
+	}
+	
+	public  Noticias BuscarNoticiaTitulo(String titulo) throws TecnicException{
+		String cadenaSQL="SELECT * from noticias WHERE TITULO =' "+titulo+ "'" ;
+		Noticias n=null;
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			if ( reg.next()){
+				n=new Noticias(titulo,reg.getString(2),reg.getString(3),reg.getString(4),reg.getInt(5));
+			}			
+			s.close();
+			this.cerrar();
+			return n;
+		}
+		catch ( SQLException e){
+		//	System.out.println(e.getMessage());
+			throw new TecnicException("En este momento no podemos atender su petici�n");
+			
+		}
+	}
+	
+	public  Noticias BuscarNoticiaFecha(String fecha) throws TecnicException{
+		String cadenaSQL="SELECT * from noticias WHERE FECHA =' "+fecha+ "'" ;
+		Noticias n=null;
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			if ( reg.next()){
+				n=new Noticias(fecha,reg.getString(2),reg.getString(3),reg.getString(4),reg.getInt(5));
+			}			
+			s.close();
+			this.cerrar();
+			return n;
+		}
+		catch ( SQLException e){
+		//	System.out.println(e.getMessage());
+			throw new TecnicException("En este momento no podemos atender su petici�n");
+			
+		}
+	}
+	
+	
+	public  Noticias BuscarNoticiaUsuario(String autor) throws TecnicException{
+		String cadenaSQL="SELECT * from noticias WHERE AUTOR =' "+autor+ "'" ;
+		Noticias n=null;
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			if ( reg.next()){
+				n=new Noticias(autor,reg.getString(2),reg.getString(3),reg.getString(4),reg.getInt(5));
+			}			
+			s.close();
+			this.cerrar();
+			return n;
+		}
+		catch ( SQLException e){
+		//	System.out.println(e.getMessage());
+			throw new TecnicException("En este momento no podemos atender su petici�n");
+			
+		}
+	}
+	
+	
+	public int AñadirNoticia(Noticias n) throws TecnicException{
+		String cadenaSQL="INSERT INTO noticias (TITULO, AUTOR, FECHA) VALUES('" + n.getTitulo() + "','" +
+				n.getAutor()+"','"+n.getFecha()+"')"; 
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+			}
+			catch ( SQLException e){			
+				return -1;
+			}
+		
+	}
+	
 
 }
--- EnteraVida/src/bbdd/BD_Temas.java
+++ EnteraVida/src/bbdd/BD_Temas.java
@@ -1,5 +1,80 @@
 package bbdd;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class BD_Temas {
+import modelos.Noticias;
+import modelos.Temas;
+import modelos.Usuarios;
 
+	
+	public class BD_Temas extends BD_Conector{
+		private static Statement s;		
+		private static ResultSet reg;
+
+		
+		public BD_Temas(){
+			super();
+		}
+		
+		
+		public int CrearTema(Temas t) throws TecnicException{
+			String cadenaSQL="INSERT INTO temas (FECHA,TITULO_FORO, TITULO_TEMA, AUTOR,DESCRIPCION) VALUES('" + t.getFecha() + "','" +
+					t.getTitulo_foro()+"','"+t.getTitulo_tema()+"','"+t.getAutor()+"','"+t.getDescripcion()+"')";
+			try{
+				this.abrir();
+				s=c.createStatement();
+				int filas=s.executeUpdate(cadenaSQL);
+				s.close();
+				this.cerrar();
+				return filas;
+				}
+				catch ( SQLException e){			
+					return -1;
+				}
+			
+		}
+		
+		public  Temas BuscarTemaTitF(String titulo_foro) throws TecnicException{
+			String cadenaSQL="SELECT * from noticias WHERE TITULO_AUTOR =' "+titulo_foro+ "'" ;
+			Temas t=null;
+			try{
+				this.abrir();
+				s=c.createStatement();
+				reg=s.executeQuery(cadenaSQL);
+				if ( reg.next()){
+					t=new Temas(reg.getInt(1),reg.getInt(2),titulo_foro,reg.getString(4),reg.getString(5),reg.getString(6),reg.getString(7));
+				}			
+				s.close();
+				this.cerrar();
+				return t;
+			}
+			catch ( SQLException e){
+			//	System.out.println(e.getMessage());
+				throw new TecnicException("En este momento no podemos atender su petici�n");
+				
+			}
+		}
+		
+		
+		public  Temas BuscarTemaTitT(String titulo_tema) throws TecnicException{
+			String cadenaSQL="SELECT * from noticias WHERE TITULO_TEMA =' "+titulo_tema+ "'" ;
+			Temas t=null;
+			try{
+				this.abrir();
+				s=c.createStatement();
+				reg=s.executeQuery(cadenaSQL);
+				if ( reg.next()){
+					t=new Temas(reg.getInt(1),reg.getInt(2),titulo_tema,reg.getString(4),reg.getString(5),reg.getString(6),reg.getString(7));
+				}			
+				s.close();
+				this.cerrar();
+				return t;
+			}
+			catch ( SQLException e){
+			//	System.out.println(e.getMessage());
+				throw new TecnicException("En este momento no podemos atender su petici�n");
+				
+			}
+		}
 }
